0c0122d6abb23b775cb9e8b72ef3ce7516e3d325
EC2 Default User <ec2-user@ip-172-31-91-248.ec2.internal>
2018-05-23 21:46:43 +0000

    nginx redirect to alpha.nodescape.io

--- docker-nginx.sh
+++ docker-nginx.sh
@@ -5,8 +5,8 @@ sudo docker run -d \
     --name nginx \
     --restart=always \
     -p 80:80 \
-    -v ~/nodescape/nginx-custom.conf:/etc/nginx/nginx-custom.conf:ro
-    -v ~/nodescape/src/:/usr/share/nginx/html:ro \
+    -v ~/nodescape/nginx-custom.conf:/etc/nginx/conf.d/nginx-custom.conf:ro \
+    -v ~/nodescape/src/:/etc/nginx/html/:ro \
     nginx
 
     # -v /nginx.conf:/etc/nginx/nginx.conf:ro \
