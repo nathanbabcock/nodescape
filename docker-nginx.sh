6aadb295e086ff8b6bff424e25a9a414482f3d77
QMCX63 <nathan.babcock@motorolasolutions.com>
2018-05-23 16:24:46 -0500

    Move node_module js dependencies to src/js

--- docker-nginx.sh
+++ docker-nginx.sh
@@ -5,7 +5,7 @@ sudo docker run -d \
     --name nginx \
     --restart=always \
     -p 80:80 \
-    -v src/:/usr/share/nginx/html:ro \
+    -v ~/nodescape/src/:/usr/share/nginx/html:ro \
     nginx
 
     # -v /nginx.conf:/etc/nginx/nginx.conf:ro \
