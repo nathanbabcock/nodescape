82c904d58ab259c81f2a82d08e82d8e7facf61bd
QMCX63 <nathan.babcock@motorolasolutions.com>
2018-05-23 16:32:31 -0500

    nginx config

--- docker-nginx.sh
+++ docker-nginx.sh
@@ -5,6 +5,7 @@ sudo docker run -d \
     --name nginx \
     --restart=always \
     -p 80:80 \
+    -v ~/nodescape/nginx-custom.conf:/etc/nginx/nginx-custom.conf:ro
     -v ~/nodescape/src/:/usr/share/nginx/html:ro \
     nginx
 
