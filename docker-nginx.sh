ae7e0b7c00647c8456d3e3debf5dbd7a7801e77e
EC2 Default User <ec2-user@ip-172-31-91-248.ec2.internal>
2018-05-24 17:28:16 +0000

    ssl

--- docker-nginx.sh
+++ docker-nginx.sh
@@ -5,8 +5,11 @@ sudo docker run -d \
     --name nginx \
     --restart=always \
     -p 80:80 \
+    -p 443:443 \
+    -v ~/nodescape/cert/:/etc/ssl/cert/ \
     -v ~/nodescape/nginx-custom.conf:/etc/nginx/conf.d/nginx-custom.conf:ro \
     -v ~/nodescape/src/:/etc/nginx/html/:ro \
     nginx
+sudo docker ps -a
 
     # -v /nginx.conf:/etc/nginx/nginx.conf:ro \
